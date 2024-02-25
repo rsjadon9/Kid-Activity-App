198bf650c092af0191e4392754b79c64e5f8e360
Rashmi Jadon <rjs.jadon@gmail.com>
2024-02-25 16:50:33 -0600

    LandingPage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styles/LandingPage.css'
 
 const LandingPage = () => {
   return (
-    <div>
-      Home
+    <div className='header'>
+      <div className='header-content'>
+        <h1 className='header-text'>Kid Activity </h1>
+        <p className='header-description'>
+        "Discover a world of fun and learning with our Kid Activity App! 🌈👧🧒
+        </p>
+      </div>
+      <div className='header-image'></div>
     </div>
   )
 }
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className='login'>
+      <div className='login-container'>
+        <h2>Login</h2>
+        <div className='form'>
+          <label htmlFor='username'>Username</label>
+          <input type="text" placeholder='Enter UserName'/>   '
+      </div>
+      <div className='form'>
+          <label htmlFor='username'>Password</label>
+          <input type="password" placeholder='Enter password'/>   '
+      </div>
+      <button className='btn-login'>Login</button>
+      </div>
     </div>
   )
 }
