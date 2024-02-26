f92bb95e07a8f2f1c43cb5d2698c32daa4ee342d
Rashmi Jadon <rjs.jadon@gmail.com>
2024-02-25 22:06:00 -0600

    save add kid changes

--- server/routes/kid.js
+++ server/routes/kid.js
@@ -3,11 +3,13 @@ import { Kid }  from "../models/Kid.js"
  const router = express.Router();
 
  router.post('/addKid' , async(req,res) =>{
-  const name = req.body.username;
-  const gender =    req.body.email;
-  const dateOfBirth =    req.body.cell;
-
-  const newKid = new Kid({name,pgenderassword,dateOfBirth});
+  const name = req.body.name;
+  const gender =    req.body.gender;
+  const dateOfBirth =    req.body.dateOfBirth;
+  console.log(name)
+  console.log(gender)
+  console.log(dateOfBirth)
+  const newKid = new Kid({name,gender,dateOfBirth});
   await newKid.save();
   res.status(201).send({ message: "New Kid Added Successfully !", success: true });
  });
