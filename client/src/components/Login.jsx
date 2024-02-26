c0d8091ff97c141b56999e81cf57c175fa82bd74
Rashmi Jadon <rjs.jadon@gmail.com>
2024-02-25 18:44:32 -0600

    Database connection establish

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () =>{
+        e.preventDefault();
+
+  }
   return (
     <div className='login'>
       <div className='login-container'>
         <h2>Login</h2>
         <div className='form'>
           <label htmlFor='username'>Username</label>
-          <input type="text" placeholder='Enter UserName'/>   
+          <input type="text" placeholder='Enter UserName' onChange={(e)=>setUsername(e.target.value)}/>   
       </div>
       <div className='form'>
           <label htmlFor='username'>Password</label>
-          <input type="password" placeholder='Enter password'/>   
+          <input type="password" placeholder='Enter password'onChange={(e)=>setPassword(e.target.value)}/>   
       </div>
-      <button className='btn-login'>Login</button>
+      <button className='btn-login'onClick={handleSubmit}>Login</button>
       </div>
     </div>
   )
---
+++ server/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
+
+const Connection = async() =>{
+    try {
+        mongoose.connect(process.env.URL) 
+        console.log("connected")
+
+    } catch (error) {
+        console.log("Error:"+err)
+        
+    }
+
+}
+
+export {Connection}
